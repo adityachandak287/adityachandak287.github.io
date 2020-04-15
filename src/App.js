ec9fd5108e892068e157e3332da56233b8adf53e
Aditya Chandak <adityachandak287@gmail.com>
2020-04-15 23:15:55 +0530

    background sketch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import BackgroundSketch from "./components/BackgroundSketch";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BackgroundSketch />
+        <div className="test">
+          <h1>Aditya Chandak</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
